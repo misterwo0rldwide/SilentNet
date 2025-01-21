a2568db2a4db360d7f56c986db696ac99196b7f2
misterwo0rldwide <78499936+misterwo0rldwide@users.noreply.github.com>
2025-01-21 21:58:31 +0200

    Started manager

--- src/manager/manager.py
+++ src/manager/manager.py
@@ -1,1 +1,24 @@
+#
+#   'silent net' Manager side.
+#   Manager can get different kinds of data
+#   About his employees with their monitored work
+#   Through requests to server which holds the data
+#
+#   Omer Kfir (C)
+import sys
+
+sys.path.append("..")
+import protocol
+
 __author__ = "Omer Kfir"
+
+def main():
+    
+    client = protocol.client()
+    client.connect(protocol.server.SERVER_BIND_IP, protocol.server.SERVER_BIND_PORT)
+
+    
+    client.close()
+
+if __name__ == "__main__":
+    main()
